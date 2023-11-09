09c521bd3a5a4a6dd775bea1a3e1da09b46a6c5e
JasurbekErgashev <jergashev633@gmail.com>
2023-11-09 17:57:37 +0500

    added test app

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final supabase = Supabase.instance.client;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['ANON_KEY']!,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Dropdown Demo Search',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Dropdown Demo Search'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: DropdownSearch<dynamic>(
+          popupProps: const PopupProps.menu(
+            showSearchBox: true,
+          ),
+          itemAsString: (dynamic data) => data['name'],
+          asyncItems: (String filter) async {
+            final data = await supabase
+                .from('customer')
+                .select()
+                .ilike('name', '%$filter%');
+            return data;
+          },
+          onChanged: (dynamic data) {
+            print(data);
+          },
+        ),
+      ),
+    );
+  }
+}
